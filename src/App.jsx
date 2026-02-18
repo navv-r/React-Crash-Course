3b5dc74945da3cf7dd3f5cf98866ad2fefea316e
navv-r <navendra604@gmail.com>
2026-02-17 20:15:42 -0500

    more stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css';
 import Todo from './components/Todo.jsx';
+import Title from './components/Title.jsx';
 
 function App() {
   return (
     <div>
-      <h1>My Todo List</h1>
+      <Title />
       <Todo />
-      <div>
-        <h2>Finish Frontend Simplified</h2>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h2>Finish Interview Section</h2>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h2>Land a 100K Job</h2>
-        <button>Delete</button>
-      </div>
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,10 @@
+function Todo() {
+    return (
+        <div>
+        <h2>Finish Frontend Simplified</h2>
+        <button>Delete</button>
+      </div>
+    )
+}
+
+export default Todo

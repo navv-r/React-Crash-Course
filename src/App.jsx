8ceade1255e1c5a176bc53ebb62abd390eb7f93c
navv-r <navendra604@gmail.com>
2026-02-20 12:29:15 -0500

    nav stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,23 @@
 import './App.css';
-import Todo from './components/Todo.jsx';
-import Title from './components/Title.jsx';
-import Modal from './components/Modal.jsx';
-import React, { useState } from 'react';
-import Counter from './components/Counter.jsx';
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';  
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Nav from './components/Nav.jsx';
 
 function App( ) {
-  const [showModal, setShowModal] = useState(false) 
-  
-  function onTodoDelete() {
-    setShowModal(true) 
-  }
-
-  function cancelModal() {
-    setShowModal(false)
-  }
-
-  function confirmModal() {
-    setShowModal(false)
-  }
-  
   return (
-    <>  
-      <Title />
-      <div>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button onClick={() => setShowModal(true)}>Add Item</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified"/>
-        <Todo onTodoDelete={onTodoDelete}  title="Learn React"/>
-        <Todo onTodoDelete={onTodoDelete}  title="Land a $100K job"/>
-      </div>
-      
-      {showModal && (
-        <Modal 
-          cancelModal={cancelModal} 
-          confirmModal={confirmModal} 
-          title="Confirm Delete?"/>
-        )}
-    </>    
-  );
-
-  
-}
+  <div>
+    <Router>
+      <Nav />
+       <Routes>
+         <Route path="/" element={<Home />} /> 
+         <Route path="/About" element={<About />} />
+         <Route path="/contact" element={<Contact />} />
+       </Routes>
+    </Router>
+  </div>)
+}  
   
 
-export default App; 
+export default App;       
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'; 
+
+function Nav() {
+    return(
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </nav>
+    )
+}
+
+export default Nav

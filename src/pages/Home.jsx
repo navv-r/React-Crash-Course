9198317b43f1e33c3afc9d20cd09ffbb5c13ed6d
navv-r <navendra604@gmail.com>
2026-02-21 15:57:48 -0500

    something

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from ".  /components/User.jsx"
 function Home() {
-    return <h1>Home</h1>
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUsers();
+    }, 500);
+  });
+
+  return (
+    <div>
+      {users.map((user) => (
+				<User 
+				name={user.name} 
+				id={user.id} 
+				email={user.email}
+				username={user.username}  
+				/>
+				))}
+    </div>
+  );
 }
 
-export default Home 
+export default Home;
